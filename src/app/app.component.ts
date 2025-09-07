0490b2adc0a021ca4ebb9b6ffa05d5ca8543099a
usamazafarawan <usamazafar111@yahoo.com>
2025-09-07 10:18:13 +0500

    added Mobile resposiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,49 @@ export class AppComponent {
   title = 'travel-app-demo';
   selectedOption: string = ''; // default nothing selected
   selectedTransportOption: string = ''; // default nothing selected
-    activeTab: string = 'tab1';
+  activeTab: string = 'tab1';
+  cards = [
+    {
+      title: 'Informatii utile',
+      open: false,
+      contentTitle: 'Informatii utile',
+      bullets: [
+        { icon: '../assets/icons/building.png', text: 'Populatie: Aproximativ 10 milioane locuitori (zona metropolitana depaseste 20 milioane)' },
+        { icon: '../assets/icons/japnese-text.png', text: 'Limba: Araba (limba oficiala), Engleza frecvent folosita in zonele turistice' },
+        { icon: '../assets/icons/cloud.png', text: 'Clima: Clima desertica, verile extrem de calde, iernile blande.' },
+        { icon: '../assets/icons/currency.png', text: 'Moneda: Lira Egipteana (EGP)' },
+        { icon: '../assets/icons/clock.png', text: 'Fus orar: UTC+02:00' }
+      ]
+    }
+    ,
 
+    {
+      title: 'Transport Public',
+      open: false,
+      contentTitle: 'Informatii utile despre Cairo, Transport public',
+      bullets: [
+        { icon: '../assets/icons/building.png', text: 'Populatie: Aproximativ 10 milioane locuitori (zona metropolitana depaseste 20 milioane)' },
+        { icon: '../assets/icons/japnese-text.png', text: 'Limba: Araba (limba oficiala), Engleza frecvent folosita in zonele turistice' },
+        { icon: '../assets/icons/cloud.png', text: 'Clima: Clima desertica, verile extrem de calde, iernile blande.' },
+        { icon: '../assets/icons/currency.png', text: 'Moneda: Lira Egipteana (EGP)' },
+        { icon: '../assets/icons/clock.png', text: 'Fus orar: UTC+02:00' }
+      ]
+    }
+    ,
+    {
+      title: 'Bucatarie Locala ',
+      open: false,
+      contentTitle: 'Informatii utile Bucatarie locala',
+      bullets: [
+        { icon: '../assets/icons/building.png', text: 'Populatie: Aproximativ 10 milioane locuitori (zona metropolitana depaseste 20 milioane)' },
+        { icon: '../assets/icons/japnese-text.png', text: 'Limba: Araba (limba oficiala), Engleza frecvent folosita in zonele turistice' },
+        { icon: '../assets/icons/cloud.png', text: 'Clima: Clima desertica, verile extrem de calde, iernile blande.' },
+        { icon: '../assets/icons/currency.png', text: 'Moneda: Lira Egipteana (EGP)' },
+        { icon: '../assets/icons/clock.png', text: 'Fus orar: UTC+02:00' }
+      ]
+    }
+    ,
+  ];
 
   setOption(option: string) {
     this.selectedOption = option;
@@ -23,5 +64,9 @@ export class AppComponent {
 
   setTab(tab: string) {
     this.activeTab = tab;
+  }
+
+  toggleCard(card: any) {
+    card.open = !card.open; // Toggle open/close
   }
 }
